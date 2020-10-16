c4ab31980caa9cb9b346821032717a9aee9c8c33
Atharva Dongare <agdongare4@gmail.com>
2020-10-17 02:57:39 +0530

    Nested if else added

--- 1.basicProgramming/2.conditionalStatement.py
+++ 1.basicProgramming/2.conditionalStatement.py
@@ -10,3 +10,15 @@ if a==10:
     print(" a is 10")
 else:
     print ("i dont know")
+
+
+if (a > 5):
+    print ("The number is greater than 5")
+    if (a > 7):
+        print("The number is greater than 7")
+    if (a > 9):
+        print ("The number is greater than 9")
+    if (a > 11):
+        print ("number is greater than 11")
+else :
+    print ("I dont know")
